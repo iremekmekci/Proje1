f224b18d5a9fae282e616b1be91439f72bbf03f2
irem <ekmekciirem9@gmail.com>
2024-11-04 20:02:59 +0300

    Add files via upload

---
+++ cpp11.cpp
@@ -0,0 +1,34 @@
+#include  <iostream>
+using namespace std ;
+
+int main(){   
+int fTemp,cTemp;
+cout <<"input tempature\"as fahrenheit\" \n";
+cin >>fTemp;
+
+cTemp=(fTemp-32)*5/9;
+
+cout << fTemp << "fahrenheit="
+<<cTemp<<"celcius"<<endl;
+
+
+return 0;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
